aeddfa9d9fe9e9c73312b4ce8e36fc0e641119b8
Wongani <wonganichulu@gmail.com>
2024-05-18 17:01:11 +0200

    Added classes Amenity, City, Place, Review, State and made all python scripts executable with chmod u+x

---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module for class Review """
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ Class that defines objects of type Review """
+    place_id = ""
+    user_id = ""
+    text = ""
