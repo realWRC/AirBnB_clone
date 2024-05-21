bc10fd3b5ab396e7e2f834b6214e95c14675977e
Wongani <wonganichulu@gmail.com>
2024-05-21 08:23:00 +0200

    Added fix to city.py

--- models/city.py
+++ models/city.py
@@ -5,11 +5,7 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """
-    Class that defines objects of type City
-        state_id = None
-        name = None
-    """
+    """Class that defines objects of type City"""
 
     state_id = ""
     name = ""
